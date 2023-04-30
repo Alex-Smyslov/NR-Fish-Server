5e0c2279452eb961f6d052f01d53bd27bf1a06aa
Alex-Smyslov <112191344+Alex-Smyslov@users.noreply.github.com>
2023-04-30 11:14:02 +0300

    22

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const products = require("./static/products.json");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 app.use(
   cors({
@@ -14,6 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
+});
+
+app.use("/", (req, res) => {
+  res.json({ mesaage: "Hello from Express Server" });
 });
 
 app.get("/catalog", async (req, res) => {
